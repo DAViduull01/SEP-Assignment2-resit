d7b120b89b944938efcedc8d3771518736f6ec23
DAViduull01 <ichimdavid01@gmail.com>
2024-07-07 16:03:01 +0300

    test and compile

---
+++ build.py
@@ -0,0 +1,15 @@
+from pybuilder.core import use_plugin, init
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.distutils")
+
+name = "your_project_name"
+version = "0.1.0"
+default_task = ["clean", "install_dependencies", "analyze", "publish"]
+
+@init
+def initialize(project):
+    project.build_depends_on("mock")
+    project.set_property("dir_source_unittest_python", "tests")
